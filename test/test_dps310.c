1ee7e4874aa572ed21e8e3511b3c1d4d7aeff92f
Otso Jousimaa <otso@ojousima.net>
2019-11-17 14:08:32 +0200

    Add missing semicolon

--- test/test_dps310.c
+++ test/test_dps310.c
@@ -12,7 +12,7 @@ void tearDown(void)
 
 void test_dps310_get_product_id(void)
 {
-  TEST_IGNORE_MESSAGE("Setting up project, ignore results")
+  TEST_IGNORE_MESSAGE("Setting up project, ignore results");
   uint8_t id = dps310_get_product_id();
   TEST_ASSERT(DPS310_PRODUCT_ID_VAL == id);
 }
